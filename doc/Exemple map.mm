333162b9a4d121ed94af5ef043362db7b3bcd42a
lilive <lilivounet@gmail.com>
2020-06-10 14:55:40 +0200

    feat: Add example map

---
+++ doc/Exemple map.mm
@@ -0,0 +1,254 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Jumper&#xa;example" LOCALIZED_STYLE_REF="styles.topic" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1591784341202" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#192441" BORDER_DASH="CLOSE_DOTS" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="6.0 pt" SHAPE_VERTICAL_MARGIN="6.0 pt"><hook NAME="MapStyle" background="#89a5e9">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" show_notes_in_map="true" edgeColorConfiguration="#353535ff,#353535ff,#353535ff,#353535ff,#353535ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="14.0 pt" MAX_WIDTH="600.0 px" COLOR="#000000" BACKGROUND_COLOR="#ffffff" BACKGROUND_ALPHA="76" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" BORDER_WIDTH="0.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<font NAME="Candara" SIZE="14"/>
+<edge STYLE="horizontal" COLOR="#192441" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="6.0 pt" SHAPE_VERTICAL_MARGIN="6.0 pt" UNIFORM_SHAPE="true" BORDER_WIDTH="4.0 px" BORDER_DASH="DASHES">
+<font NAME="Raleway" SIZE="14" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Ligne 1" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#ff4a4a" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 2" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#ffc265">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#ffa14a" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 3" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#feff62">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#fff24a" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 4" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#7dff72">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#69ff4a" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 5" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#61fff2">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#4affe4" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 6" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#6973ff">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#6973ff" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Ligne 7" BACKGROUND_COLOR="#cccccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="600.0 px" BORDER_WIDTH="3.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#ff6df2">
+<font NAME="Raleway" SIZE="14"/>
+<edge COLOR="#ff4af8" WIDTH="3"/>
+</stylenode>
+<stylenode TEXT="Code" BACKGROUND_COLOR="#ffffff" BACKGROUND_ALPHA="0" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="2000.0 px" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#000000" BORDER_COLOR_ALPHA="50">
+<font NAME="Source Code Pro" SIZE="12"/>
+</stylenode>
+<stylenode TEXT="Raccourci clavier" BACKGROUND_COLOR="#ffffff" BACKGROUND_ALPHA="0" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="4.0 pt" MAX_WIDTH="2000.0 px" BORDER_WIDTH="2.0 px" BORDER_COLOR="#000000" BORDER_COLOR_ALPHA="129" BORDER_DASH="CLOSE_DOTS">
+<font NAME="Source Code Pro" SIZE="12" BOLD="false" ITALIC="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="136" RULE="ON_BRANCH_CREATION"/>
+<edge WIDTH="thin"/>
+<node TEXT="Projects" STYLE_REF="Ligne 1" FOLDED="true" POSITION="right" ID="ID_1216447785" CREATED="1591786232822" MODIFIED="1591788410291">
+<font BOLD="true"/>
+<node TEXT="Go to the moon" STYLE_REF="Ligne 1" FOLDED="true" ID="ID_343368502" CREATED="1591786247846" MODIFIED="1591788083677">
+<node TEXT="Find a rocket" ID="ID_1695672999" CREATED="1591786255606" MODIFIED="1591786259450"/>
+<node TEXT="Get a proper suit" ID="ID_962889803" CREATED="1591786259974" MODIFIED="1591786267034"/>
+<node TEXT="Bring some tea" ID="ID_450957114" CREATED="1591786267542" MODIFIED="1591786273145"/>
+<node TEXT="Write my will" ID="ID_1480534960" CREATED="1591786277974" MODIFIED="1591786327609"/>
+</node>
+<node TEXT="Holidays" STYLE_REF="Ligne 1" FOLDED="true" ID="ID_825660812" CREATED="1591786331398" MODIFIED="1591788084474">
+<node TEXT="Winter" FOLDED="true" ID="ID_624178734" CREATED="1591786336630" MODIFIED="1591786339033">
+<node TEXT="Nord pole" FOLDED="true" ID="ID_1197334077" CREATED="1591786343014" MODIFIED="1591786351719">
+<node TEXT="I would like to meet a penguin" ID="ID_349996908" CREATED="1591786360326" MODIFIED="1591786396394"/>
+<node TEXT="I&apos;m a little concerned about the temperature" ID="ID_716343010" CREATED="1591786406934" MODIFIED="1591786420954"/>
+<node TEXT="Bring some really hot tea" ID="ID_1994395175" CREATED="1591786421910" MODIFIED="1591786430457"/>
+</node>
+<node TEXT="Africa" FOLDED="true" ID="ID_1639950710" CREATED="1591786432134" MODIFIED="1591786439562">
+<node TEXT="What about a trip to Africa next Christmas ?" ID="ID_1301596661" CREATED="1591786441846" MODIFIED="1591786524201"/>
+<node TEXT="Todo: Do some researches about Christmas in other cultures" ID="ID_839312517" CREATED="1591786525063" MODIFIED="1591786574538"/>
+</node>
+</node>
+<node TEXT="Summer" FOLDED="true" ID="ID_1661737457" CREATED="1591786339462" MODIFIED="1591786341849">
+<node TEXT="Surf" FOLDED="true" ID="ID_459004185" CREATED="1591786577623" MODIFIED="1591786585577">
+<node TEXT="I would like to learn this !" ID="ID_1211304143" CREATED="1591786586198" MODIFIED="1591786613959"/>
+<node TEXT="I have to learn to swim first" ID="ID_458381904" CREATED="1591786614423" MODIFIED="1591786624841"/>
+<node TEXT="Save some money for swim lessons" ID="ID_1634127849" CREATED="1591786626359" MODIFIED="1591786658857"/>
+<node TEXT="Find a nice spot, perhaps some paradise island" ID="ID_1190865244" CREATED="1591787628055" MODIFIED="1591787650459"/>
+</node>
+<node TEXT="Girlfriends" ID="ID_1031764850" CREATED="1591786662567" MODIFIED="1591788475039">
+<icon BUILTIN="bookmark"/>
+<font BOLD="true"/>
+<node TEXT="I think summer can be a good season to find someone" FOLDED="true" ID="ID_868350186" CREATED="1591786687638" MODIFIED="1591792889185">
+<node ID="ID_1150841588" CREATED="1591786714807" MODIFIED="1591789781032"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      I have to do some search about <b>ultraviolet rays</b>.
+    </p>
+    <p>
+      Is it good for the skin ? <b>Is it risky ?</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Not sure, but learn how to play guitar may be an asset" ID="ID_1670541920" CREATED="1591786825959" MODIFIED="1591786893066"/>
+</node>
+</node>
+</node>
+<node TEXT="Write my memories" STYLE_REF="Ligne 1" FOLDED="true" ID="ID_534700818" CREATED="1591786899382" MODIFIED="1591788402456" HGAP_QUANTITY="28.99999955296518 pt" VSHIFT_QUANTITY="22.499999329447768 pt">
+<node TEXT="To do later, I&apos;m need to live more to make an interesting book" ID="ID_1359471608" CREATED="1591786909415" MODIFIED="1591786943631"/>
+</node>
+<node TEXT="Travels" STYLE_REF="Ligne 1" FOLDED="true" ID="ID_1801322762" CREATED="1591787589208" MODIFIED="1591788410290" VSHIFT_QUANTITY="29.999999105930357 pt">
+<node ID="ID_1693214883" TREE_ID="ID_343368502">
+<node ID="ID_1504275315" TREE_ID="ID_1695672999"/>
+<node ID="ID_776420524" TREE_ID="ID_962889803"/>
+<node ID="ID_1886965900" TREE_ID="ID_450957114"/>
+<node ID="ID_1904600262" TREE_ID="ID_1480534960"/>
+</node>
+<node ID="ID_1465650576" TREE_ID="ID_1197334077">
+<node ID="ID_839204848" TREE_ID="ID_349996908"/>
+<node ID="ID_654437500" TREE_ID="ID_716343010"/>
+<node ID="ID_1660190945" TREE_ID="ID_1994395175"/>
+</node>
+<node ID="ID_604404695" TREE_ID="ID_1639950710">
+<node ID="ID_417011463" TREE_ID="ID_1301596661"/>
+<node ID="ID_1985041981" TREE_ID="ID_839312517"/>
+</node>
+<node ID="ID_1128447502" TREE_ID="ID_1190865244"/>
+</node>
+</node>
+<node TEXT="Food" STYLE_REF="Ligne 3" FOLDED="true" POSITION="right" ID="ID_220538901" CREATED="1591786949719" MODIFIED="1591788093097">
+<font BOLD="true"/>
+<node TEXT="What do I like ?" STYLE_REF="Ligne 3" ID="ID_613281358" CREATED="1591786953590" MODIFIED="1591788235213">
+<node TEXT="Pepperoni" ID="ID_37696076" CREATED="1591786963383" MODIFIED="1591786981887"/>
+<node TEXT="Tea" ID="ID_529823722" CREATED="1591786984343" MODIFIED="1591788605320">
+<icon BUILTIN="bookmark"/>
+<font BOLD="true"/>
+<node TEXT="Green tea is my favorite" ID="ID_1866935231" CREATED="1591787100327" MODIFIED="1591787117738"/>
+<node TEXT="Nothing with fruits or flowers inside" ID="ID_294607356" CREATED="1591787118663" MODIFIED="1591787170027"/>
+<node TEXT="Girls find tea sophisticated. This is a good point." ID="ID_1900489830" CREATED="1591787863559" MODIFIED="1591790041865">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Potatoes" ID="ID_463310947" CREATED="1591786987158" MODIFIED="1591786996874"/>
+</node>
+<node TEXT="What do I hate ?" STYLE_REF="Ligne 3" FOLDED="true" ID="ID_1265557498" CREATED="1591787003463" MODIFIED="1591788237836">
+<node TEXT="Coffee" ID="ID_516571129" CREATED="1591787016583" MODIFIED="1591787030106"/>
+<node TEXT="Artichoke" FOLDED="true" ID="ID_847338020" CREATED="1591787030615" MODIFIED="1591787054798">
+<node ID="ID_91095139" CREATED="1591787055447" MODIFIED="1591788211126"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      They make me <b>sick</b>&nbsp;every time
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Fruits" FOLDED="true" ID="ID_11617567" CREATED="1591787174807" MODIFIED="1591787178314">
+<node TEXT="Apart strawberry that I really like" ID="ID_704320757" CREATED="1591787179463" MODIFIED="1591792272546"/>
+</node>
+</node>
+</node>
+<node TEXT="Philosophy" STYLE_REF="Ligne 6" FOLDED="true" POSITION="right" ID="ID_1764903035" CREATED="1591787233511" MODIFIED="1591788380179">
+<font BOLD="true"/>
+<node TEXT="The death" STYLE_REF="Ligne 6" FOLDED="true" ID="ID_743649520" CREATED="1591787257415" MODIFIED="1591788254472">
+<icon BUILTIN="bookmark"/>
+<font BOLD="true"/>
+<node TEXT="Rule number 1 : Do not fear it" ID="ID_1563707968" CREATED="1591787273047" MODIFIED="1591788315672">
+<font BOLD="true"/>
+</node>
+<node TEXT="Rule number 2 : Do not fear it" ID="ID_1204006461" CREATED="1591787273047" MODIFIED="1591788316343">
+<font BOLD="true"/>
+</node>
+<node TEXT="Rule number 3 : Not a reason to take unnecessary risks" ID="ID_1250035259" CREATED="1591787307478" MODIFIED="1591788317546">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Love" STYLE_REF="Ligne 6" FOLDED="true" ID="ID_1280344252" CREATED="1591787385479" MODIFIED="1591788375387" HGAP_QUANTITY="36.49999932944777 pt" VSHIFT_QUANTITY="29.999999105930357 pt">
+<icon BUILTIN="bookmark"/>
+<font BOLD="true"/>
+<node TEXT="Try to love everybody and anything" ID="ID_1817574223" CREATED="1591787389303" MODIFIED="1591789297760">
+<icon BUILTIN="yes"/>
+</node>
+<node TEXT="But it&apos;s really hard" ID="ID_1323801804" CREATED="1591787412342" MODIFIED="1591788341847">
+<icon BUILTIN="messagebox_warning"/>
+<font ITALIC="true"/>
+</node>
+<node TEXT="Solution : avoid persons and places that you don&apos;t like" ID="ID_1985574157" CREATED="1591787429207" MODIFIED="1591787486890"/>
+<node TEXT="Love can be more than to have a girlfriend" FOLDED="true" ID="ID_1351288936" CREATED="1591787834152" MODIFIED="1591787858795">
+<node TEXT="How to meet a girlfriend ?" ID="ID_1382087880" CREATED="1591788873512" MODIFIED="1591788952428">
+<node TEXT="Tip 1" ID="ID_691659169" CREATED="1591788920248" MODIFIED="1591788924651">
+<node ID="ID_898571535" TREE_ID="ID_1900489830"/>
+</node>
+<node TEXT="Tip 2" ID="ID_507231339" CREATED="1591788982392" MODIFIED="1591788984860">
+<node ID="ID_1461836176" TREE_ID="ID_868350186">
+<node ID="ID_1154282147" TREE_ID="ID_1150841588"/>
+</node>
+</node>
+<node TEXT="Tip 3" ID="ID_1196054025" CREATED="1591789028712" MODIFIED="1591789031516">
+<node ID="ID_490374922" TREE_ID="ID_1670541920"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="That&apos;s essentially what I&apos;m concerned about" ID="ID_16639158" CREATED="1591788256776" MODIFIED="1591788380177" VSHIFT_QUANTITY="22.499999329447768 pt"/>
+</node>
+</node>
+</map>
