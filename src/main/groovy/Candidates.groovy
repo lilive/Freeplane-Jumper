e4cd6f8ba9539ef8010beef9d50e6d6c2780a054
lilive <lilivounet@gmail.com>
2020-06-23 14:44:35 +0200

    feat: interrupt the previous running filter to start the new one

--- src/main/groovy/Candidates.groovy
+++ src/main/groovy/Candidates.groovy
@@ -6,19 +6,27 @@ import javax.swing.DefaultListModel
 import javax.swing.SwingUtilities
 
 /**
- * Carry the datas for the matching nodes GUI list (a JList)
- * To refresh the list, call update()
- * (I need this Model instead of the default one to be able to refresh the whole
- *  GUI list in one shot, because it can be a lot of nodes and refresh the GUI
- *  one node after another was too slow)
+ * Carry the nodes to search, and the result list of matching nodes
+ * for the GUI JList.
+ * I need this Model instead of the default one to be able to
+ * refresh the whole GUI list in one shot, because refresh the
+ * GUI one node after another was too slow)
  */
 class Candidates extends DefaultListModel<SNode>{
     
     private SNodes candidates = []
     private SNodes results = []
-    private int numMax = 100
+    private int numMax = 30
     private Thread searchThread
 
+    /**
+     * Defined the nodes where to search and run a first search.
+     * @param candidates The nodes where to search.
+     * @param pattern The search string. @see filter()
+     * @param options @see filter()
+     * @param mandatoryNode @see filter()
+     * @param onDone @see filter()
+     */
     void set(
         SNodes candidates,
         String pattern,
@@ -29,12 +37,14 @@ class Candidates extends DefaultListModel<SNode>{
         this.candidates = candidates
         filter( pattern, options, mandatoryNode, onDone )
     }
-    
+
+    // Return a result for the last search
     @Override
     SNode getElementAt( int idx ){
         return results[ idx ]
     }
-    
+
+    // Return the last search results size
     @Override
     int getSize(){
         if( results ) return results.size()
@@ -54,9 +64,18 @@ class Candidates extends DefaultListModel<SNode>{
     }
     
     /**
-     * Update the nodes displayed in the GUI, according to a search pattern.
-     * @param pattern The mask to filter all the searched nodes.
-     *                This string is interpreted as one or many regex seperated by a space.
+     * Set the results list with the candidates that match a search pattern.
+     * @param pattern The search string.
+     * @param options Settings for the search strategy.
+     *          Regex or not ? Case sensitive ? Etc.
+     *          @see SearchOptions
+     * @param mandatoryNode If the search stops because numMax
+     *          nodes are found, be sure to search also in this node. This
+     *          allow to always have the map currently selected node in
+     *          the results if it match the pattern.
+     * @param onDone Closure to call after the search.
+     *          Useful because the search is done in a new Thread.
+     *          onDone will be executed in the Swing EDT.
      */
     void filter(
         String pattern,
@@ -64,7 +83,11 @@ class Candidates extends DefaultListModel<SNode>{
         SNode mandatoryNode,
         Closure onDone
     ){
-        if( searchThread?.isAlive() ) searchThread.join()
+        // If the previous filter is still processing, stop it
+        if( searchThread?.isAlive() ){
+            searchThread.interrupt()
+            searchThread.join()
+        }
         
         // Reset the search results for all nodes in the map
         if( candidates ) candidates[0].sMap.each{ it.clearPreviousSearch() }
@@ -72,8 +95,7 @@ class Candidates extends DefaultListModel<SNode>{
         // Get the differents units in the pattern
         Set<String> units = makePatternUnits( pattern, options )
         if( ! pattern ){
-            update( candidates  )
-            onDone()
+            applyFilterResults( candidates, onDone )
             return
         }
 
@@ -81,21 +103,31 @@ class Candidates extends DefaultListModel<SNode>{
 
         searchThread = new Thread( new Runnable(){
             public void run(){
+                boolean done = true
+                SNodes results
                 // Get the candidates that match
-                SNodes results = regexFilter( units, candidates, options, mandatoryNode )
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        // Update the results
-                        // Get all the nodes that match the patterns
-                        update( results )
-                        onDone()
-                    }
-                })
+                try{
+                    results = regexFilter( units, candidates, options, mandatoryNode )
+                } catch (InterruptedException e) {
+                    done = false
+                }
+                // Update the object
+                if( done ) applyFilterResults( results, onDone )
             }
-        })
+        }, "filter thread" )
+        
         searchThread.start()
     }
 
+    /**
+     * Split the search string by whitespaces, to get units to search in nodes.
+     * @param pattern The search string.
+     * @param options In some cases the pattern is not splitted, and the
+     *                returned list will contains only one element:
+     *                the whole pattern.
+     * @return The pattern splitted by whitespaces or a single element Set,
+     *         according to options.
+     */
     private Set<String> makePatternUnits( String pattern, SearchOptions options ){
         
         pattern = pattern.trim()
@@ -112,13 +144,26 @@ class Candidates extends DefaultListModel<SNode>{
             return [ pattern ]
         }
     }
-    
-    private SNodes regexFilter( Set<String> units, SNodes candidates, SearchOptions options, SNode mandatoryNode ){
+
+    /**
+     * Return the candidates that match a list of pattern units.
+     * @param units One or more strings to search in the candidates.
+     * @param options Settings for the search strategy.
+     *          Regex or not ? Case sensitive ? Etc.
+     *          The units are converted to regex according to
+     *          this parameter.
+     *          @see SearchOptions
+     * @param mandatoryNode @see filter()
+     */
+    private SNodes regexFilter(
+        Set<String> units, SNodes candidates,
+        SearchOptions options, SNode mandatoryNode
+    ) throws InterruptedException {
 
         boolean oneValidRegex = false
         Set<Pattern> regexps = []
 
-        // Convert patterns to regex
+        // Convert units to regular expressions, according to options
         try {
             regexps.addAll( units.collect{
                 String exp = it
@@ -131,12 +176,12 @@ class Candidates extends DefaultListModel<SNode>{
             } )
         } catch (PatternSyntaxException e) {}
 
-        // Keep all candidates if the pattern contains only invalid regex
+        // Return all candidates if the pattern contains only invalid regex
         if( ! oneValidRegex ) return candidates
         
-        // Get the candidates that match the regex
+        // Get the candidates that match the regular expressions.
         // Don't get more than numMax results, but be sure that
-        // the currently selected node is searched
+        // the currently selected node is searched.
         SNodes results = new SNodes()
         boolean maxReached = false
         candidates.each{
@@ -145,14 +190,28 @@ class Candidates extends DefaultListModel<SNode>{
                 results << it
                 maxReached = ( results.size() >= numMax - 1 )
             }
+            if (Thread.interrupted()) throw new InterruptedException()
         }
         
         return results
     }
 
-    // Set the results
+    
+    // Update the object with these results, and call a post opration.
+    // All is done inside the Swing EDT.
+    private void applyFilterResults( SNodes results, Closure after ){
+        SwingUtilities.invokeLater( new Runnable(){
+            public void run() {
+                update( results )
+                after()
+            }
+        })
+    }
+
+    // Update the object with these results and fire the events
+    // for the GUI update.
     private void update( SNodes newResults ){
-        
+
         if( getSize() > 0 ) fireIntervalRemoved( this, 0, getSize() - 1 )
 
         if( newResults.size() <= numMax ){
