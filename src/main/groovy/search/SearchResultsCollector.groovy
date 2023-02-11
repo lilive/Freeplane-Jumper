c86210a23b6413a07250741f5e164074d93e528f
lilive <lilivve@yahoo.fr>
2023-02-11 14:40:11 +0100

    doc: add comments

--- src/main/groovy/search/SearchResultsCollector.groovy
+++ src/main/groovy/search/SearchResultsCollector.groovy
@@ -2,7 +2,20 @@ package lilive.jumper.search
 
 import lilive.jumper.data.SNodes
 
+/**
+ * Object that can receive the results of a search.
+ */
 interface SearchResultsCollector {
+    
+    /**
+     * Add this results to the list of results.
+     * @param newResults The results to add.
+     * @param done True if the search is complete.
+     */
     void addResults( SNodes newResults, boolean done )
+    
+    /**
+     * Clear all the received results.
+     */
     void clearResults()
 }
