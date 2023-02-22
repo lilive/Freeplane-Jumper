614576a8b2c78d1bbe01b30b9a5076585d7ef5e2
lilive <lilivve@yahoo.fr>
2023-02-22 10:41:07 +0100

    doc: add comments

--- src/main/groovy/search/SearchOptions.groovy
+++ src/main/groovy/search/SearchOptions.groovy
@@ -5,15 +5,18 @@ package lilive.jumper.search
  */
 class SearchOptions implements Cloneable {
     
-    boolean useRegex = false
-    boolean caseSensitive = false
-    boolean fromStart = false
-    boolean splitPattern = true
-    boolean transversal = true
-    boolean useDetails = true
-    boolean useNote = true
-    boolean useAttributesName = true
-    boolean useAttributesValue = true
+    boolean useRegex = false          // Use regular expressions
+    boolean caseSensitive = false     // Case (in)sensitive search
+    boolean fromStart = false         // Search the pattern at the beginning of the text
+    boolean splitPattern = true       // Split the pattern into words (or smaller regular expressions).
+                                      // A node is considering to match if it contains all of them, in any order.
+                                      // Ignored if transversal is true.
+    boolean transversal = true        // Find nodes that don't match the entire pattern if their
+                                      // ancestors match the rest of the pattern
+    boolean useDetails = true         // Search into the nodes details
+    boolean useNote = true            // Search into the nodes note
+    boolean useAttributesName = true  // Search into the attributes name
+    boolean useAttributesValue = true // Search into the attributes value
 
     public boolean allDetailsTrue(){
         return useDetails && useNote && useAttributesName && useAttributesValue
